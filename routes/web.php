5fde3f9cdf0742643863dedb3e2dee00d8b28bc1
s3a317097 <s3a317097@users.noreply.github.com>
2018-01-11 17:19:18 +0800

    checkage 建立

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ Route::get('/', function () {
 Route::get('/2',function(){
     throw new \Exception('Tracyworks!');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
